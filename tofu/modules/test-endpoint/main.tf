d75287f7997f4b88ad2188d764b707053c29f1fa
Mozo953 <thomasgharali@gmail.com>
2026-01-09 20:37:21 +0100

    fix: creation physique de l'arborescence modules

--- tofu/modules/test-endpoint/main.tf
+++ tofu/modules/test-endpoint/main.tf
@@ -2,10 +2,6 @@ data "http" "test_endpoint" {
   url = var.endpoint
 }
 
-variable "endpoint" {
-  description = "The endpoint to test"
-  type        = string
-}
 
 output "status_code" {
   value = data.http.test_endpoint.status_code
